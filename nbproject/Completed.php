ebce4e8799de564a14f23dd5325b4bced65a327a
SafetyCube <fleming.adam206@gmail.com>
2015-12-10 22:27:58 -0500

    Completed ORM object

---
+++ nbproject/Completed.php
@@ -0,0 +1,39 @@
+<?php
+/** @Entity @Table(name="completed")
+ */
+class Completed {
+    /** @ID @Column(type="integer") */
+    private $cid;
+    /** @Column(type="integer") */
+    private $rid;
+    /** @Column(type="date") */
+    private $date;
+    /** @Column(type="integer") */
+    private $finalValue;
+    
+    public function getCid() {
+        return $this->cid;
+    }
+    public function setCid($cid){
+        $this->cid = $cid;
+    }
+    public function getFinalValue() {
+        return $this->finalValue;
+    }
+    public function setFinalValue($finalValue) {
+        $this->finalValue = $finalValue;
+    }
+    public function getDate() {
+        return $this->date;
+    }
+    public function setDate($date) {
+        $this->date = $date;
+    }
+    public function finalValue() {
+        return $this->finalValue;
+    }
+    public function setFinalValue($finalValue) {
+        $this->finalValue = $finalValue;
+    }
+}
+
